4dbc37808892d0d61e8c6f757916ba71dd20730a
V_Borysenko <preinspiron@gmail.com>
2023-09-22 15:15:54 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Container from './modules/shared/container/container';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename="/your-pet-front">
     <React.StrictMode>
-      <App />
+      <Container>
+        <App />
+      </Container>
     </React.StrictMode>
   </BrowserRouter>
 );
