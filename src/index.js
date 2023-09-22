f8aa344f17f70b3d3bd9d133535d71bcda47216e
Favixx <bogdandom52@gmail.com>
2023-09-22 15:29:58 +0300

    Added RTK, Formik, yup, AuthForm markup, redux store created

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename="/your-pet-front">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 );
