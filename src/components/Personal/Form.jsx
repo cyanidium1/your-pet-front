611b54efa76118f4b288629b96b1475dc7caaed2
V_Borysenko <preinspiron@gmail.com>
2023-09-23 19:25:03 +0200

    3

--- src/components/Personal/Form.jsx
+++ src/components/Personal/Form.jsx
@@ -1,71 +1,92 @@
 import { Formik, Field, Form } from 'formik';
 import scss from './personal.module.scss';
+import defualtPhoto from '../../images/icons.svg';
 
-export const PersonalForm = () => {
+export const PersonalForm = ({ mode }) => {
   return (
     <Formik
       initialValues={{
         firstName: '',
-        lastName: '',
         email: '',
+        birthday: '',
+        toggledEditPhoto: false,
       }}
       onSubmit={async values => {
         await new Promise(r => setTimeout(r, 500));
         alert(JSON.stringify(values, null, 2));
       }}
     >
-      <Form className={scss.form}>
-        <label htmlFor="firstName" className={scss.label}>
-          Name:
-          <Field
-            className={scss.input}
-            id="firstName"
-            name="firstName"
-            placeholder=""
-          />
-        </label>
+      {({ values }) => (
+        <Form className={scss.form}>
+          {mode && (
+            <label className={scss.editPhotoBlock}>
+              <Field type="checkbox" name="toggledEditPhoto"></Field>
+              <svg className={scss.editPhoto}>
+                <use href={`${defualtPhoto}#icon-camera`}></use>
+              </svg>
+              Edit photo {`${values.toggledEditPhoto}`}
+            </label>
+          )}
 
-        <label htmlFor="email" className={scss.label}>
-          Email:
-          <Field
-            id="email"
-            name="email"
-            placeholder=""
-            type="email"
-            className={scss.input}
-          />
-        </label>
+          <label htmlFor="firstName" className={scss.label}>
+            Name:
+            <Field
+              className={scss.input}
+              id="firstName"
+              name="firstName"
+              placeholder="Anna"
+            />
+          </label>
 
-        <label htmlFor="birthday" className={scss.label}>
-          Birthday:
-          <Field
-            id="birthday"
-            name="birthday"
-            placeholder=""
-            type="date"
-            className={scss.input}
-          />
-        </label>
+          <label htmlFor="email" className={scss.label}>
+            Email:
+            <Field
+              id="email"
+              name="email"
+              placeholder="anna@gmail.com"
+              type="email"
+              className={scss.input}
+            />
+          </label>
 
-        <label htmlFor="phone" className={scss.label}>
-          Phone:
-          <Field
-            id="phone"
-            name="phone"
-            placeholder="+38"
-            type="phone"
-            className={scss.input}
-          />
-        </label>
+          <label htmlFor="birthday" className={scss.label}>
+            Birthday:
+            <Field
+              id="birthday"
+              name="birthday"
+              placeholder=""
+              type="date"
+              className={scss.input}
+            />
+          </label>
 
-        <label htmlFor="city" className={scss.label}>
-          City:
-          <Field id="city" name="city" placeholder="" className={scss.input} />
-        </label>
-        <button className={scss.button} type="submit">
-          Save
-        </button>
-      </Form>
+          <label htmlFor="phone" className={scss.label}>
+            Phone:
+            <Field
+              id="phone"
+              name="phone"
+              placeholder="+38000000000"
+              type="phone"
+              className={scss.input}
+            />
+          </label>
+
+          <label htmlFor="city" className={scss.label}>
+            City:
+            <Field
+              id="city"
+              name="city"
+              placeholder="Kiev"
+              className={scss.input}
+            />
+          </label>
+          {mode && (
+            <button className={scss.button} type="submit">
+              Save
+            </button>
+          )}
+        </Form>
+      )}
     </Formik>
   );
 };
--- src/components/Personal/PersonalCard.jsx
+++ src/components/Personal/PersonalCard.jsx
@@ -2,7 +2,11 @@ import scss from './personal.module.scss';
 import defualtPhoto from '../../images/icons.svg';
 import { PersonalForm } from './Form';
 import { Formik, Field, Form } from 'formik';
+import { useState } from 'react';
 export const Card = () => {
+  const [editMode, SetEditMode] = useState(false);
+  const edit = () => SetEditMode(!editMode);
+
   return (
     <div>
       <h2>My information:</h2>
@@ -11,14 +15,25 @@ export const Card = () => {
           <svg className={scss.personalPhoto}>
             <use href={`${defualtPhoto}#icon-Photo-default`}></use>
           </svg>
-          <button>
+          <button onClick={edit}>
             <svg className={scss.personalPhotoEdit}>
-              <use href={`${defualtPhoto}#icon-edit`}></use>
+              <use
+                href={`${defualtPhoto}#icon-${
+                  !editMode ? 'edit' : 'cross-small'
+                }`}
+              ></use>
             </svg>
           </button>
         </div>
-
-        <PersonalForm />
+        <PersonalForm mode={editMode} />
+        {!editMode && (
+          <div className={scss.logoutblock}>
+            <svg className={scss.logout}>
+              <use href={`${defualtPhoto}#icon-logout`}></use>
+            </svg>
+            <span>Log Out</span>
+          </div>
+        )}
       </div>
     </div>
   );
