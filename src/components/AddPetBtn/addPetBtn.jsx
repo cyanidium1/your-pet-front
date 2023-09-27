8858beb9525b387106636b0155537412864ff95e
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-09-27 19:26:48 +0300

    created addBtn

--- src/components/AddPetBtn/addPetBtn.jsx
+++ src/components/AddPetBtn/addPetBtn.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { addNewPet } from "redux/myPets/addPetOperations";
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import css from './addPetBtn.module.css';
+import sprite from '../../images/icons.svg';
 
 const AddPetBtn = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/add-pet");
-  };
-  return <button onClick={handleClick}>add pet</button>;
+  return (
+    <Link to="/add-pet" className={css.addBtn}>
+      Add pet
+      <svg width="24px" height="24px" stroke="#fff">
+        <use href={`${sprite}#icon-plus-small`}></use>
+      </svg>
+    </Link>
+  );
 };
 
 export default AddPetBtn;
