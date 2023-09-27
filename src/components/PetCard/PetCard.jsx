bba42e116d64ff36dea73906bcfcef853c3ac91e
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-09-27 11:46:02 +0200

    main page full

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import styles from "./PetCard.module.css";
-import sprite from "../../images/icons.svg";
-import Button from "../../UI/Button/Button";
+import React from 'react';
+import styles from './PetCard.module.css';
+import sprite from '../../images/icons.svg';
+import Button from '../../UI/Button/Button';
 
 const PetCard = ({ info }) => {
   const { location, type, mileage, gender, isFav } = info;
@@ -9,29 +9,29 @@ const PetCard = ({ info }) => {
     <li className={styles.item}>
       <div className={styles.card}>
         <div className={styles.topParams}>
-          <p className={styles.type}>{type}</p>      
+          <p className={styles.type}>{type}</p>
           <svg className={isFav ? styles.heartIcon : styles.heartIconActive}>
-            <use href={sprite + "#heart"} />
-          </svg>       
+            <use href={sprite + '#heart'} />
+          </svg>
         </div>
         <div className={styles.bottomParams}>
           <div className={styles.parameter}>
             <svg className={styles.icon}>
-              <use href={sprite + "#location"} />
+              <use href={sprite + '#location'} />
             </svg>
             <p>
-              {location.length > 4 ? location.slice(0, 3) + "..." : location}
+              {location.length > 4 ? location.slice(0, 3) + '...' : location}
             </p>
           </div>
           <div className={styles.parameter}>
             <svg className={styles.icon}>
-              <use href={sprite + "#mileage"} />
+              <use href={sprite + '#mileage'} />
             </svg>
             <p>{mileage.length > 4 ? mileage.slice(0, 6) : mileage}</p>
           </div>
           <div className={styles.parameter}>
             <svg className={styles.icon}>
-              <use href={sprite + "#gender"} />
+              <use href={sprite + '#gender'} />
             </svg>
             <p>{gender}</p>
           </div>
@@ -39,7 +39,7 @@ const PetCard = ({ info }) => {
       </div>
       <p className={styles.info}>Сute dog looking for a home</p>
       <div className={styles.btn}>
-        <Button text={"Learn more"} />
+        <Button text={'Learn more'} />
       </div>
     </li>
   );
