f0633ba7518ad9aa0c298bc1343413058eed1335
Vyachuk <Slavkoka045@gmail.com>
2023-09-30 02:48:51 +0300

    ADD: google auth

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,14 @@
 import Main from 'components/Main/Main';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useNavigate, useSearchParams } from 'react-router-dom';
 import { addToken } from 'redux/auth/authSlice';
 
 const MainPage = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const productName = searchParams.get('token') ?? null;
-  if (productName) {
-    dispatch(addToken({ token: productName }));
-    searchParams.forEach((value, key) => {
-      searchParams.delete(key);
-    });
-    setSearchParams('');
-  }
+  const [searchParams] = useSearchParams();
+  const productName = searchParams.get('token') || null;
+  dispatch(addToken({ token: productName }));
+
   return <Main />;
 };
 
