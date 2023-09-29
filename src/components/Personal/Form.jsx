36d608c563ab596384d0b530d1c079c9e8eba813
V_Borysenko <preinspiron@gmail.com>
2023-09-29 08:41:42 +0200

    123

--- src/components/Personal/Form.jsx
+++ src/components/Personal/Form.jsx
@@ -1,8 +1,10 @@
 import { Formik, Field, Form } from 'formik';
 import scss from './personal.module.scss';
 import defualtPhoto from '../../images/icons.svg';
+import { useState } from 'react';
 
-export const PersonalForm = ({ mode, photo }) => {
+export const PersonalForm = ({ mode }) => {
+  const [file, setFile] = useState();
   return (
     <Formik
       enctype="multipart/form-data"
@@ -20,23 +22,41 @@ export const PersonalForm = ({ mode, photo }) => {
         alert(JSON.stringify(values, null, 2));
       }}
     >
-      {({ values }) => (
+      {({ values, handleChange, isValid }) => (
         <Form className={scss.form}>
-          {mode && (
-            <label className={scss.editPhotoBlock}>
-              <Field
-                onChange={e => photo(e.target.files[0])}
-                accept="image/*,image/jpeg"
-                className={scss.fileField}
-                type="file"
-                name="toggledEditPhoto"
-              ></Field>
-              <svg className={scss.editPhoto}>
-                <use href={`${defualtPhoto}#icon-camera`}></use>
-              </svg>
-              Edit photo
-            </label>
-          )}
+          <label className={scss.editPhotoBlock}>
+            <div>
+              {file ? (
+                <img
+                  src={file}
+                  alt="Selected img"
+                  className={scss.editPhotoBlock}
+                />
+              ) : (
+                <svg className={scss.personalPhoto}>
+                  <use href={`${defualtPhoto}#icon-Photo-default`}></use>
+                </svg>
+              )}
+            </div>
+            <Field
+              onChange={e => {
+                setFile(URL.createObjectURL(e.target.files[0]));
+                handleChange(e);
+              }}
+              accept="image/*,image/jpeg"
+              className={scss.fileField}
+              type="file"
+              name="toggledEditPhoto"
+            ></Field>
+            {mode && (
+              <div className={scss.editPhotoLabel}>
+                <svg className={scss.editPhoto}>
+                  <use href={`${defualtPhoto}#icon-camera`}></use>
+                </svg>
+                <span>Edit photo</span>
+              </div>
+            )}
+          </label>
 
           <label htmlFor="firstName" className={scss.label}>
             Name:
@@ -48,16 +68,6 @@ export const PersonalForm = ({ mode, photo }) => {
               disabled={!mode}
             />
           </label>
-          {/* <label htmlFor="firstName" className={scss.label}>
-            Name:
-            <Field
-              className={scss.input}
-              id="firstName"
-              name="firstName"
-              placeholder="Anna"
-              type="file"
-            />
-          </label> */}
 
           <label htmlFor="email" className={scss.label}>
             Email:
@@ -70,7 +80,6 @@ export const PersonalForm = ({ mode, photo }) => {
               disabled={!mode}
             />
           </label>
-
           <label htmlFor="birthday" className={scss.label}>
             Birthday:
             <Field
@@ -82,7 +91,6 @@ export const PersonalForm = ({ mode, photo }) => {
               disabled={!mode}
             />
           </label>
-
           <label htmlFor="phone" className={scss.label}>
             Phone:
             <Field
@@ -94,7 +102,6 @@ export const PersonalForm = ({ mode, photo }) => {
               disabled={!mode}
             />
           </label>
-
           <label htmlFor="city" className={scss.label}>
             City:
             <Field
--- src/components/Personal/PersonalCard.jsx
+++ src/components/Personal/PersonalCard.jsx
@@ -1,30 +1,17 @@
 import scss from './personal.module.scss';
 import defualtPhoto from '../../images/icons.svg';
 import { PersonalForm } from './Form';
-import { Formik, Field, Form } from 'formik';
 import { useState } from 'react';
-import { resetAuthState } from 'redux/auth/authSlice';
 export const Card = () => {
   const [editMode, SetEditMode] = useState(false);
-  const [photo, SetPhoto] = useState({ photo: { result: '' } });
-  const edit = () => SetEditMode(!editMode);
 
-  const handlePhotoEdit = photo => {
-    const reader = new FileReader();
-    reader.readAsDataURL(photo);
-    console.log(reader.photo.result);
-    return SetPhoto({ photo: reader });
-  };
+  const edit = () => SetEditMode(!editMode);
 
   return (
     <div>
       <h2>My information:</h2>
       <div className={scss.card}>
         <div id="photo">
-          <img src={photo.photo.result}></img>
-          <svg className={scss.personalPhoto}>
-            <use href={`${defualtPhoto}#icon-Photo-default`}></use>
-          </svg>
           <button onClick={edit}>
             <svg className={scss.personalPhotoEdit}>
               <use
@@ -35,7 +22,7 @@ export const Card = () => {
             </svg>
           </button>
         </div>
-        <PersonalForm mode={editMode} photo={handlePhotoEdit} />
+        <PersonalForm mode={editMode} />
         {!editMode && (
           <div className={scss.logoutblock}>
             <svg className={scss.logout}>
