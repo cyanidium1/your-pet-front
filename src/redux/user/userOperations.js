16e827769b42eb233895a02540ac548239137baf
V_Borysenko <preinspiron@gmail.com>
2023-09-30 14:27:10 +0200

    333

--- src/redux/user/userOperations.js
+++ src/redux/user/userOperations.js
@@ -23,59 +23,13 @@ export const userUpdate = createAsyncThunk(
         'api/users/edit-profile',
         formData,
         {
-          //   headers: {
-          //     'Content-Type': 'multipart/form-data',
-          //   },
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
         }
       );
       console.log(222);
       return response.data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
-
-export const addNewPetNotice = createAsyncThunk(
-  'addPet/newPetNotice',
-  async (pet, { rejectWithValue }) => {
-    try {
-      const formData = new FormData();
-
-      formData.append('name', pet.name);
-      formData.append('location', pet.location);
-      formData.append('file', pet.file);
-      formData.append('type', pet.type);
-      formData.append('date', pet.date);
-      formData.append('sex', pet.sex);
-      formData.append('title', pet.title);
-      formData.append('comments', pet.comments);
-      formData.append('category', pet.category);
-      if (pet.price) {
-        formData.append('price', pet.price);
-      }
-      console.log(formData);
-
-      const response = await addPetInstance.post('api/notices', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
-
-export const deletePet = createAsyncThunk(
-  'addPet/deletePet',
-  async (pet, { rejectWithValue }) => {
-    try {
-      const { data } = await addPetInstance.delete(`api/pets/${pet.id}`);
-
-      return data;
     } catch (error) {
       return rejectWithValue(error.message);
     }
