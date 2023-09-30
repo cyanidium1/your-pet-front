1f5aa0e9fb77e51f4983f5a5ce154c8a674935ff
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-10-01 00:31:38 +0300

    update validation

--- src/redux/myPets/addPetOperations.js
+++ src/redux/myPets/addPetOperations.js
@@ -16,8 +16,9 @@ export const addNewPet = createAsyncThunk(
       formData.append('petImage', pet.file);
       formData.append('type', pet.type);
       formData.append('dateOfBirth', pet.date);
-      formData.append('comments', pet.comments);
-
+      if (pet.comments) {
+        formData.append('comments', pet.comments);
+      }
       const response = await addPetInstance.post('api/pets', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
