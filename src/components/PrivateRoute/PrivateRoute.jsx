f01c023b537b2f941e80eb6c96cecca7c5f3d7a1
V_Borysenko <preinspiron@gmail.com>
2023-10-04 01:18:39 +0200

    to main

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsAuth } from 'redux/auth/authSelectors';
+
+const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+  // useLogCheck();
+  const isAuth = useSelector(selectIsAuth);
+
+  return !isAuth ? <Navigate to={redirectTo} /> : <Component />;
+};
+
+export default PrivateRoute;
