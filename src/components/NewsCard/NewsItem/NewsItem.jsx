ecdbfbff194aefeb012d671d797230711ad7d668
Vyachuk <Slavkoka045@gmail.com>
2023-10-04 16:38:12 +0300

    FIX: news page item infodrmation

--- src/components/NewsCard/NewsItem/NewsItem.jsx
+++ src/components/NewsCard/NewsItem/NewsItem.jsx
@@ -1,32 +1,30 @@
 import css from '../NewsItem/NewsItem.module.css';
 
 const NewsItem = ({ _id, text, image_url, title, url, date }) => {
- const updateData = new Date(date).toLocaleDateString().split(".").join("/")
- 
-    return(
+  const updateData = new Date(date).toLocaleDateString().split('.').join('/');
 
-    <li
-key={_id}
-className={`${css.item}`}
->
-<img src={image_url} alt={title} className={css.img} />
-<div className={css.textBox}>
-  <h3 className={css.title}>{title}</h3>
-  <p className={css.text}>{text}</p>
-  <div className={css.dateBox}>
-    <p className={css.date}>{updateData}</p>
-    <a
-      className={css.link}
-      href={url}
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <p>Read more</p>
-    </a>
-  </div>
-</div>
-</li>)
+  return (
+    <li key={_id} className={`${css.item}`}>
+      <img src={image_url} alt={title} className={css.img} />
+      <div className={css.textBox}>
+        <div className={css.textBoxWrapper}>
+          <h3 className={css.title}>{title}</h3>
+          <p className={css.text}>{text}</p>
+        </div>
+        <div className={css.dateBox}>
+          <p className={css.date}>{updateData}</p>
+          <a
+            className={css.link}
+            href={url}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <p>Read more</p>
+          </a>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default NewsItem;
-
